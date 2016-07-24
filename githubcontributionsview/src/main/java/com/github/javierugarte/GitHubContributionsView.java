f21d85812b7ecbfaeaecc57a4d46bc4527e0f650
Javier González <javierugarte91@gmail.com>
2016-07-25 00:49:43 +0200

    Changed sample

--- githubcontributionsview/src/main/java/com/github/javierugarte/GitHubContributionsView.java
+++ githubcontributionsview/src/main/java/com/github/javierugarte/GitHubContributionsView.java
@@ -24,7 +24,8 @@ public class GitHubContributionsView extends ImageView {
 
     private int baseColor = Color.parseColor("#d6e685"); // default color of GitHub
     private int textColor = Color.BLACK;
-    private boolean displayText = true;
+    private boolean displayMonth = false;
+    private String username = "";
 
     public GitHubContributionsView(Context context) {
         super(context);
@@ -44,39 +45,55 @@ public class GitHubContributionsView extends ImageView {
     }
 
     public void setBaseColor(String baseColor) {
-        this.baseColor = Color.parseColor(baseColor);
+        try {
+            this.baseColor = Color.parseColor(baseColor);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        refresh();
     }
 
     public void setBaseColor(int color) {
         this.baseColor = color;
+        refresh();
     }
 
     public void setTextColor(String textColor) {
-        this.textColor = Color.parseColor(textColor);
+        try {
+            this.textColor = Color.parseColor(textColor);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        refresh();
     }
 
     public void setTextColor(int textColor) {
         this.textColor = textColor;
+        refresh();
     }
 
-    public void displayText(boolean displayText) {
-        this.displayText = displayText;
+    public void displayMonth(boolean displayMonth) {
+        this.displayMonth = displayMonth;
+        refresh();
     }
 
-    public void loadUserName(String userName) {
-        loadUserName(userName, null);
+    public void loadUserName(String username) {
+        loadUserName(username, null);
     }
 
-    public void loadUserName(String userName, final OnContributionsListener listener) {
+    public void loadUserName(String username, final OnContributionsListener listener) {
+        this.username = username;
+
+        clearContribution();
 
         ContributionsRequest contributionsRequest =
                 new ContributionsRequest(getContext());
 
-        contributionsRequest.launchRequest(userName, new OnContributionsRequestListener() {
+        contributionsRequest.launchRequest(username, new OnContributionsRequestListener() {
             @Override
             public void onResponse(List<ContributionsDay> contributionsDay) {
                 Bitmap bitmap = BitmapUtils.getContributionsBitmap(contributionsDay, getMeasuredWidth(), baseColor,
-                        textColor, displayText);
+                        textColor, displayMonth);
 
                 setImageBitmap(bitmap);
 
@@ -93,6 +110,15 @@ public class GitHubContributionsView extends ImageView {
             }
         });
 
+    }
+
+    public void clearContribution() {
+        invalidate();
+        setImageBitmap(null);
+    }
+
+    private void refresh() {
+        loadUserName(this.username);
     }
 
 }
--- sample/src/main/java/com/github/javierugarte/githubcontributionssample/MainActivity.java
+++ sample/src/main/java/com/github/javierugarte/githubcontributionssample/MainActivity.java
@@ -2,52 +2,62 @@ package com.github.javierugarte.githubcontributionssample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Switch;
 
 import com.github.javierugarte.GitHubContributionsView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GitHubContributionsView mContributionView;
+
+    private EditText mUsernameEditText;
+    private Button mSearchUser;
+
+    private EditText mBaseColorEditText;
+    private Button mChangeColorButton;
+
+    private Switch mSwitchMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // JavierUgarte
-        GitHubContributionsView contributionJavierugarte =
+        mContributionView =
                 (GitHubContributionsView) findViewById(R.id.github_contributions_view);
-        if (contributionJavierugarte != null) {
-            contributionJavierugarte.setBaseColor("#d6e685");
-            contributionJavierugarte.displayText(true);
-            contributionJavierugarte.loadUserName("javierugarte");
-        }
+
+        mUsernameEditText = (EditText) findViewById(R.id.et_username);
+        mSearchUser = (Button) findViewById(R.id.btn_search);
+        mSearchUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContributionView.loadUserName(mUsernameEditText.getText().toString());
+            }
+        });
+
+        mSearchUser.performClick();
 
 
-        // Alorma
-        GitHubContributionsView contributionAlorma =
-                (GitHubContributionsView) findViewById(R.id.github_contributions_view2);
-        if (contributionAlorma != null) {
-            contributionAlorma.setBaseColor("#FF8A80");
-            contributionAlorma.loadUserName("alorma");
-        }
+        mBaseColorEditText = (EditText) findViewById(R.id.et_base_color);
+        mChangeColorButton = (Button) findViewById(R.id.btn_color);
+        mChangeColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContributionView.setBaseColor(mBaseColorEditText.getText().toString());
+            }
+        });
 
-        // Mike Penz
-        GitHubContributionsView contributionMikePenz =
-                (GitHubContributionsView) findViewById(R.id.github_contributions_view3);
-        if (contributionMikePenz != null) {
-            contributionMikePenz.setBaseColor("#EA80FC");
-            contributionMikePenz.displayText(true);
-            contributionMikePenz.loadUserName("mikepenz");
-        }
-
-        // JakeWharton
-        GitHubContributionsView contributionJakeWharton =
-                (GitHubContributionsView) findViewById(R.id.github_contributions_view4);
-        if (contributionJakeWharton != null) {
-            contributionJakeWharton.setBaseColor("#8C9EFF");
-            contributionJakeWharton.displayText(false);
-            contributionJakeWharton.loadUserName("JakeWharton");
-        }
-
+        mSwitchMonth = (Switch) findViewById(R.id.switch_display_month);
+        mSwitchMonth.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mContributionView.displayMonth(isChecked);
+            }
+        });
 
     }
 }
